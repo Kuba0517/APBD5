ca6fe522d1b8e86777ea7d0d5d70b12ae0cacb51
Jakub <jakub.ratusz@gmail.com>
2024-04-06 15:01:34 +0200

    Add initial models

---
+++ APBD5/Models/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace APBD5.Models;
+
+public class Animal
+{
+    public int ID { get; set; }
+    public string Imie { get; set; }
+    public AnimalType AnimalType { get; set; }
+    public double Weight { get; set; }
+    public string FurColor { get; set; }
+}
+
+
+public enum AnimalType
+{
+    Dog,
+    Cat,
+    Tiger,
+    Lion,
+    Shark
+}
---
+++ APBD5/Models/Visit.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD5.Models;
+
+public class Visit
+{
+    public DateTime Date { get; set; }
+    public Animal Animal { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+}
